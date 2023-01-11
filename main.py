877624ef6e8a269216703638c21bb54208af9960
ganeshkadu1 <ganeshkadu20@gmail.com>
2023-01-11 07:04:19 +0530

    Handle a exception in code when user did not enter a alpha words

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ while not WANT_TO_EXIT:
     data = pandas.read_csv("Phonetic_Alphabet.csv")
     phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
     user_input = input("Enter a word? ").upper()
-    if user_input == "EXIT":
-        WANT_TO_EXIT = True
-    else:
-        output_list = [phonetic_dic[letter] for letter in user_input]
-        print(output_list)
+    try:
+        if user_input == "EXIT":
+            WANT_TO_EXIT = True
+        else:
+            output_list = [phonetic_dic[letter] for letter in user_input]
+            print(output_list)
+    except KeyError:
+        print("Sorry, Only letter in the alphabet please")
